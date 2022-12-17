1e274146ccf740e79a5f6521d369bc5b604b1948
Erik <101416162+Eriknpy@users.noreply.github.com>
2022-12-17 14:02:11 +0100

    create : TickerService
    Implements ITicketService

---
+++ RepositoryServicePattern/RepositoryServicePattern/Services/TicketService.cs
@@ -0,0 +1,19 @@
+﻿using RepositoryServicePattern.Models;
+using RepositoryServicePattern.Repositories;
+
+namespace RepositoryServicePattern.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ITicketRepository _ticketRepo;
+
+        public TicketService(ITicketRepository ticketRepo)
+        {
+            _ticketRepo = ticketRepo;
+        }
+        public List<Ticket> GetAllSold()
+        {
+            return _ticketRepo.GetAllSold();
+        }
+    }
+}
