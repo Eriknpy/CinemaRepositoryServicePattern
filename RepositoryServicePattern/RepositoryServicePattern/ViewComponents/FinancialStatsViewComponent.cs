59fdc939e3d0133631d3c6360d6b4d56bf5878b1
Erik <101416162+Eriknpy@users.noreply.github.com>
2022-12-17 14:46:25 +0100

    create : PartialView for FinancialStatsComponent

---
+++ RepositoryServicePattern/RepositoryServicePattern/ViewComponents/FinancialStatsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepositoryServicePattern.Services;
+
+namespace RepositoryServicePattern.ViewComponents
+{
+    public class FinancialStatsViewComponent : ViewComponent
+    {
+        private readonly IFinancialsService _financialService;
+
+        public FinancialStatsViewComponent(IFinancialsService financialService)
+        {
+            _financialService = financialService;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var stats = _financialService.GetStats();
+            return Task.FromResult<IViewComponentResult>(View(stats));
+        }
+    }
+}
