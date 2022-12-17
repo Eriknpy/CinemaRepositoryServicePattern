28e8b5806a57c9a8bc2bdf0e128e04e2eca33d1f
Erik <101416162+Eriknpy@users.noreply.github.com>
2022-12-17 11:59:43 +0100

    create : MVC Project files

---
+++ RepositoryServicePattern/RepositoryServicePattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepositoryServicePattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
